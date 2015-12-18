1df6a3d0a60b466c01c8068847f2ec6edd2bdd98
Hai LS <hails@vccloud.vn>
2015-12-18 14:38:10 +0700

    Command options to execute program

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"jdict/lib/jmdict"
+	"os"
 )
 
+// Search & print out result
+func searchAndPrint(key string) {
+	result := jmdict.Query(key)
+	b, _ := json.MarshalIndent(result, "", "  ")
+	fmt.Println(string(b))
+}
+
 func main() {
-	fmt.Println("Populate Database ...")
-	jmdict.PopulateData("data/sample.dict")
+	args := os.Args[1:]
+	if len(args) > 0 {
+		if args[0] == `populate` {
+			// Populate db
+			fmt.Println("Populate Database ...")
+			jmdict.PopulateData("data/sample.dict")
+			return
+		} else if args[0] == `search` {
+			// Query word
+			if len(args) > 1 {
+				key := args[1]
+				searchAndPrint(key)
+				return
+			}
+		}
+	}
+	// Default helper
+	fmt.Println(`Usage: main [action] [options]`)
 }
